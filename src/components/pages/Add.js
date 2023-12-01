1e76f431c0b040893bef7da6c2d87c0f025682cf
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-02 00:20:26 +0100

    Update Add.js

--- src/components/pages/Add.js
+++ src/components/pages/Add.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import PersonIcon from "@mui/icons-material/Person";
-// import PublicIcon from "@mui/icons-material/Public";
-// import PaymentIcon from "@mui/icons-material/Payment";
+import { Link } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+import PublicIcon from "@mui/icons-material/Public";
+import PaymentIcon from "@mui/icons-material/Payment";
 
 const Add = () => {
   console.log("Add Page");
