67587dedfd9cc9b79725bdbb7cbf18c3fc710231
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-02 00:20:04 +0100

    add section opsætning

--- src/components/pages/Add.js
+++ src/components/pages/Add.js
@@ -1,5 +1,18 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import PersonIcon from "@mui/icons-material/Person";
+// import PublicIcon from "@mui/icons-material/Public";
+// import PaymentIcon from "@mui/icons-material/Payment";
+
 const Add = () => {
   console.log("Add Page");
-  return <h1>Add</h1>;
+  return (
+    <>
+      <h1>Add a new entry</h1>
+      <br />
+      <p>What would you like to add?</p>
+      <br />
+    </>
+  );
 };
 export default Add;
