c1c7148bda6d8625c86863dbe04d10c89554d8aa
Andre-Hot <123978520+Andre-Hot@users.noreply.github.com>
2023-12-03 23:06:07 +0100

    add details to sponsor

--- src/components/pages/Sponsors.js
+++ src/components/pages/Sponsors.js
@@ -1,5 +1,12 @@
 const Sponsors = () => {
+
+  const sponsorDetails = {
+    name: 'Sponsor Name',
+    email: 'sponsor@email',
+
+  };
   console.log("Sponsors Page");
   return <h1>Sponsors</h1>;
 };
 export default Sponsors;
+
