8bfe03a57198c16c0acaade8c8297eab58baa8eb
Andre-Hot <123978520+Andre-Hot@users.noreply.github.com>
2023-12-04 13:34:57 +0100

    New details

--- src/components/pages/Sponsors.js
+++ src/components/pages/Sponsors.js
@@ -1,10 +1,19 @@
+import React from "react";
 const Sponsors = () => {
 
-  const sponsorDetails = {
-    name: 'Sponsor Name',
-    email: 'sponsor@email',
+  const sponsor = [
+    { Sub: 'Kenya', id: 'K1023', name: 'Kirsten Hansen', email: 'placeholder@periamma.org', Periamma: 'cust-0138', Donations: '', Active: true },
+    { Sub: 'Kenya', id: 'K2386', name: 'Martin Jensen', email: 'Martin-j@havton', Periamma: 'cust-0821', Donations: '', Active: true },
+    { Sub: 'Kenya', id: 'K6499', name: 'Adrianna Danielka', email: 'Ad.da@company.dk', Periamma: 'cust-1650', Donations: 'cust-6557', Active: true },
+    { Sub: 'Kenya', id: 'K4085', name: 'Lucia Garofalo', email: 'lucia-garofalo@outlook.com', Periamma: '', Donations: 'cust-3789', Active: true },
+    { Sub: 'Kenya', id: 'K0433', name: 'Sobieslaw Bulgarelli', email: 'sobieslaw.bulga@gmail.com', Periamma: 'cust-6070', Donations: '', Active: true },
+    { Sub: 'Kenya', id: 'K4641', name: 'Khasan Nacimbene', email: 'doctorhasan@clinic.org', Periamma: 'cust-9080', Donations: '', Active: true },
+    { Sub: 'Kenya', id: 'K6420', name: 'Markius Hoffsted', email: 'thebestbrewer@beer.dk', Periamma: 'cust-6139', Donations: 'cust-2067', Active: true },
+    { Sub: 'Kenya', id: '4391', name: 'Anders Martinsen', email: 'am.am@anders.org', Periamma: 'cust-7682', Donations: 'cust-0387', Active: false },
+    { Sub: 'Kenya', id: 'K7724', name: 'Peter Møller', email: 'peter.moller@stenhusene.dk', Periamma: 'cust-6854', Donations: '', Active: true },
 
-  };
+  ];
+  
 
   return (
     <div>
@@ -12,8 +21,8 @@ const Sponsors = () => {
 
     <div>
     <h2>Sponsor Details</h2>
-    <p>Name: {sponsorDetails.name}</p>
-    <p>Email: {sponsorDetails.email}</p>
+    <p>Name: {sponsor.name}</p>
+    <p>Email: {sponsor.email}</p>
       </div>
       </div>
   )
