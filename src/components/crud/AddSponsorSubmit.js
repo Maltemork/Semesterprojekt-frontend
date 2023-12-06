31d2326e2090127c31c32e0d7f413163cc03a24c
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-06 18:15:52 +0100

    POST request sponsor

--- src/components/crud/AddSponsorSubmit.js
+++ src/components/crud/AddSponsorSubmit.js
@@ -1,5 +1,23 @@
 const HandleSponsorFormData = (data) => {
   console.log("Sponsor Form Input Data:", data);
+
+  fetch("https://periamma-projekt-app.azurewebsites.net/sponsors/add", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Response from POST request to sponsors failed.");
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("POST request successful", responseData);
+    })
+    .catch((err) => {
+      console.log("POST request failed", err);
+    });
 };
 
 export default HandleSponsorFormData;
