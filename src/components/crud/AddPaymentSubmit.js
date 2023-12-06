bc32a1d5ce6bb291f8ce32063efe0c98d098f195
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-06 18:20:18 +0100

    POST request payments

--- src/components/crud/AddPaymentSubmit.js
+++ src/components/crud/AddPaymentSubmit.js
@@ -1,5 +1,23 @@
 const HandlePaymentFormData = (data) => {
   console.log("Payment Form Input Data:", data);
+
+  fetch("https://periamma-projekt-app.azurewebsites.net/payments/add", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Response from POST request to payments failed.");
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("POST request successful", responseData);
+    })
+    .catch((err) => {
+      console.log("POST request failed", err);
+    });
 };
 
 export default HandlePaymentFormData;
