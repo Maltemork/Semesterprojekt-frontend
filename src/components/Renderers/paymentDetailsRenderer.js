8794b916da1d782486104087bc5b445c6ba56ee9
Maltemork <malte.sorensen@protonmail.com>
2023-12-07 17:02:24 +0100

    small change

--- src/components/Renderers/paymentDetailsRenderer.js
+++ src/components/Renderers/paymentDetailsRenderer.js
@@ -7,28 +7,26 @@ import GroupIcon from "@mui/icons-material/Group";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-const SponsorDetailsRenderer = ({ sponsorObject }) => {
+const SponsorDetailsRenderer = ({ sponsorObject: paymentObject }) => {
     const navigate = useNavigate();
 
     function handleEditButton() {
-        
-        console.log(`edit for ${sponsorObject.name} clicked`);
         navigate("./edit");
     }
     function handleDeleteButton() {
-        console.log(`delete for ${sponsorObject.name} clicked`)
+        console.log(`delete for ${paymentObject.id} clicked`)
     }
 
     return (
         <div className="detail-view-sponsor">
-            <h1 className="detail-view-title">Invoice ID: { sponsorObject.id }</h1>
+            <h1 className="detail-view-title">Invoice ID: { paymentObject.id }</h1>
             <div className="detail-card">
                 <GroupIcon />
-                <h2>Customer: { sponsorObject.customer }</h2>
-                <h2>Subscription: { sponsorObject.subscription }</h2>
-                <h2>Amount: { sponsorObject.amount }</h2>
-                <h2>Plan: { sponsorObject.planHandle }</h2>
-                <h2>Created: { new Date(sponsorObject.created).toString() }</h2>
+                <h2>Customer: { paymentObject.customer }</h2>
+                <h2>Subscription: { paymentObject.subscription }</h2>
+                <h2>Amount: { paymentObject.amount }</h2>
+                <h2>Plan: { paymentObject.planHandle }</h2>
+                <h2>Created: { new Date(paymentObject.created).toString() }</h2>
             </div>
             <Stack direction="row" spacing={2} className="detail-buttons">
                 <Button variant="contained" onClick={handleEditButton}>Edit</Button>
