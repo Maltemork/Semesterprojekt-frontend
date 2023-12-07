f806902a3010050fb9497b0fa38a0bc2db24d2f2
Andre-Hot <123978520+Andre-Hot@users.noreply.github.com>
2023-12-07 22:34:14 +0100

    added few details

--- src/components/pages/sponsorDetails.js
+++ src/components/pages/sponsorDetails.js
@@ -1,13 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export default function SponsorDetails() {
 
     const { id } = useParams()
+    const sponsor = useLoaderData()
     return (
         <div className="sponsor-details">
-            <h2> {id}</h2>
+            <h2> Sponsor Details for {sponsor.title}</h2>
+            <p>Sponsor Full Name: {sponsor.name}</p>
+            <p>Sponsor Email: {sponsor.email}</p>
+            <p>Sponsor Private/business: {sponsor.state}</p>
+            <p>Sponsor CPR/CVR: {sponsor.active}</p>
+            <div className="details">
+                <p></p>
+        </div>
         
         </div>
         
     );
+}
+
+export const careerDetailsLoader = async ({ params }) => {
+    const { id } = params
+
+    const res = await fetch("http://localhost:3000/sponsors" + id);
+
+    return res.json()
 }
