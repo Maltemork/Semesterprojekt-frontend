923df177d25ff3990bbaddc872981172eeab93f9
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-13 01:00:39 +0100

    Update getData.js

--- src/components/crud/getData.js
+++ src/components/crud/getData.js
@@ -2,31 +2,33 @@ const endpoint = "https://periamma-projekt-app.azurewebsites.net";
 
 // Get data based on 'type' (could be "sponsors", "children" or "payments").
 async function getData(type) {
-    // Get data from endpoint.
-    const response = await fetch(`${endpoint}/${type}`);
-    const data = await response.json();
-    // Map data into an array.
-    const dataArray = Object.keys(data).map(key => ({ id: key, ...data[key] }));
-    // Return the mapped data.
-    return dataArray;
+  // Get data from endpoint.
+  const response = await fetch(`${endpoint}/${type}`);
+  const data = await response.json();
+  // Map data into an array.
+  const dataArray = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
+  // Return the mapped data.
+  return dataArray;
 }
 
 // Get a specific object
 async function getObject(table, objectId) {
-    const response = await fetch(`${endpoint}/${table}/${objectId}`);
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${endpoint}/${table}/${objectId}`);
+  const data = await response.json();
+  return data;
 }
 
 // Delete a specific object
 async function deleteObject(table, objectId) {
-    await fetch(`${endpoint}/${table}/${objectId}/delete`, {
-        method: "DELETE",
-    });
+  await fetch(`${endpoint}/${table}/${objectId}/delete`, {
+    method: "DELETE",
+  });
 }
 
-export {
-    getData,
-    getObject,
-    deleteObject
-};
+async function getSponsorPayments(sponsorId) {
+  const response = await fetch(`${endpoint}/sponsors/${sponsorId}/payments`);
+  const data = await response.json();
+  return data;
+}
+
+export { getData, getObject, deleteObject, getSponsorPayments };
