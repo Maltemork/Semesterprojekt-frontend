76820b77940409f6e890f7e43ba00e51886a4c8c
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-12-14 16:47:42 +0100

    Update childObject.js

--- src/components/Renderers/childObject.js
+++ src/components/Renderers/childObject.js
@@ -1,33 +1,36 @@
 export function ConstructChildObject(childdata) {
-    const ChildObject = {
-        id: childdata.id,
-        childNo: childdata.childNo,
-        subitems: childdata.subitems,
-        sponsor: childdata.sponsor,
-        perviousSponsor: childdata.sponsoredBy,
-        additionalSponsor: childdata.secondSponsor,
-        name: childdata.fullname,
-        gender: childdata.gender,
-        birthdate: childdata.birthdate,
-        type: childdata.type,
-        school: childdata.school,
-        schoolStart: childdata.schoolStart,
-        class: childdata.class,
-        sponsoredBy: childdata.sponsoredBy,
-        secondSponsor: childdata.secondSponsor,
-        notes: childdata.notes,
-        getAge() {
-            let date = new Date(childdata.birthdate);
-            let age = Math.floor((Date.now() - date.getTime()) / (365.25 * 24 * 60 * 60 * 1000));
-            return age;
-        }
-    }
+  const ChildObject = {
+    id: childdata.id,
+    childNo: childdata.childNo,
+    subitems: childdata.subitems,
+    sponsor: childdata.sponsor,
+    perviousSponsor: childdata.sponsoredBy,
+    additionalSponsor: childdata.secondSponsor,
+    name: childdata.fullname,
+    gender: childdata.gender,
+    birthdate: childdata.birthdate,
+    type: childdata.type,
+    school: childdata.school,
+    schoolStart: childdata.schoolStart,
+    class: childdata.class,
+    sponsoredBy: childdata.sponsoredBy,
+    secondSponsor: childdata.secondSponsor,
+    notes: childdata.notes,
+    getAge() {
+      let date = new Date(childdata.birthdate);
+      let age = Math.floor(
+        (Date.now() - date.getTime()) / (365.25 * 24 * 60 * 60 * 1000)
+      );
+      return age;
+    },
+  };
 
-    Object.defineProperty(ChildObject, "childNo", {
-        configurable: false,
-        writable: false
-      });
+  function getSponsors() {}
 
-    return ChildObject;
+  Object.defineProperty(ChildObject, "childNo", {
+    configurable: false,
+    writable: false,
+  });
+
+  return ChildObject;
 }
-
